76306913af63c4ce81e022694fb7136d60149df3
Ramesh Errabolu <Ramesh.Errabolu@amd.com>
2022-10-03 13:07:58 -0500

    Adding some debug related cheat sheets

--- LinuxKernel/LnxKrnlContainerOf.c
+++ LinuxKernel/LnxKrnlContainerOf.c
@@ -1,6 +1,8 @@
 
 Linux kernel provides a service called "container_of". Users can use this service
-to discover the base address of an instance given the address of a member field.
+to discover the BASE ADDRESS of a STRUCT INSTANCE when given the address of a member
+field of this struct.
+
 The concept and usefulness of this service is best understood via an illustration.
 
 Assume the following:
@@ -10,21 +12,22 @@ Assume the following:
       bool boo;
       list_head idList;
       uint64_t moo;
-      char *coo;
+      char *rama_namam;
       struct randData rand;
       list_head dataList;
     };
 
 Assume user has the address of a string object that is guaranteed to represent
-the member "coo". User wants to know the value of boolean flag "boo" that is
-part of the same instance of someDataStruct. One can use the container_of macro
+the member "rama_namam". User wants to know the value of boolean flag "boo" that
+is part of the same instance of someDataStruct. One can use the container_of macro
 provided by Linux kernel to accomplish this task.
 
   // Obtain the base address of struct someDataStruct instance given the
-  // address of member filed "coo". The address of member field "coo" is
-  // represented by the object "cooAddr" in code block below
+  // address of member filed "rama_namam". The address of member field
+  // "rama_namam" is represented by the object "rama_namam_addr" in code
+  // block below
   struct someDataStruct *someDataAddr;
-  someDataAddr = container_of(cooAddr, struct someDataStruct, coo);
+  someDataAddr = container_of(rama_namam_addr, struct someDataStruct, rama_namam);
 
   // Simply read the value of "boo" field
   bool booValue = someDataAddr->boo;
